4b57d1669a360d8d49d46933788ecfbb0cfb6be0
bilal-abraham <bilal.abraham.717@gmail.com>
2021-12-19 09:59:42 -0800

    navbar menu fix

--- client/src/components/layout/HomeHero.jsx
+++ client/src/components/layout/HomeHero.jsx
@@ -11,9 +11,9 @@ const HomeHero = () => {
                 <br /><br />
                 <img className='home-hero-img' src={Logo} alt='cemetery-logo'/>
                 <br /><br />
-                <header className='home-hero-header'>The Evergreen Project</header>
+                <header className='home-hero-header'>The Evergreen Cemetery</header>
                 <br /><br />
-                <p className='home-hero-subtext'>Flutter, Firebase, JavaScript, Node.js</p>
+                <p className='home-hero-subtext'>Subtext, Subtext, Subtext</p>
                 <br />
                 <br />
                 <br />
