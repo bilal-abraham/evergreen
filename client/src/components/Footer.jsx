0f27f451aa4cab639e7c0512c2959b535eda91ea
bilal-abraham <bilal.abraham.717@gmail.com>
2021-12-19 10:40:51 -0800

    home hero almost finished (just need to add button to about us && footer complete just not gonna be displayed on the home page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,8 +6,13 @@ import './css/Footer.css'
 const Footer = () => {
     return (
         <Fragment>
-            <section className='footer'>
-                <h1 className='footer-text'>Footer</h1>
+            <section className="footer-container">
+                <p className="copyright">The Evergreen Project © 2021</p>
+                <div className="social-icons">
+                    <a href="https://github.com/bilal-abraham/evergreen">
+                        <i className="fab fa-github i" />
+                    </a>
+                </div>
             </section>
         </Fragment>
     )
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,10 +4,9 @@ import { ReactComponent as Temple } from '../components/svg/temple.svg'
 import { ReactComponent as About } from '../components/svg/about.svg'
 import { ReactComponent as ArrowDown } from '../components/svg/arrow-down.svg'
 import Navbar from '../components/NavBar.jsx'
-import Hero from '../components/layout/HomeHero.jsx'
-import Footer from '../components/Footer.jsx'
 import NavItem from '../components/NavItem.jsx'  
 import DropdownMenu from '../components/DropdownMenu.jsx'
+import Hero from '../components/layout/HomeHero.jsx'
 
 
 const Home = () => {
@@ -22,7 +21,6 @@ const Home = () => {
                 </NavItem>
             </Navbar>
             <Hero />
-            <Footer />
         </Fragment>
     )
 }
