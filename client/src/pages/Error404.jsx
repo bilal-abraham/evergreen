1e9d1acf885e57448c0ef55a8f0b62fe2573b343
bilal-abraham <bilal.abraham.717@gmail.com>
2021-12-20 16:24:31 -0800

    creating depoloy script for fast deploying in ./client/package.json

--- client/src/pages/Error404.jsx
+++ client/src/pages/Error404.jsx
@@ -1,16 +1,14 @@
-import React, { Fragment } from 'react'
-import NavBar from '../components/NavBar'
-import ErrorHero from '../components/layout/ErrorHero'
-import Footer from '../components/Footer'
-
+import React, { Fragment } from 'react';
+import ErrorHero from '../components/layout/ErrorHero';
+import Footer from '../components/Footer';
 
 const Error404 = () => {
-    return (
-        <Fragment>
-            <ErrorHero />
-            <Footer />
-        </Fragment>
-    )
-}
+	return (
+		<Fragment>
+			<ErrorHero />
+			<Footer />
+		</Fragment>
+	);
+};
 
-export default Error404
+export default Error404;
