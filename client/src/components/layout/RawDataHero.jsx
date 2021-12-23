d8122ee0a1fa6b75477110762c7330797ba34501
bilal-abraham <bilal.abraham.717@gmail.com>
2021-12-23 12:10:36 -0800

    raw data basics are finsihed

--- client/src/components/layout/RawDataHero.jsx
+++ client/src/components/layout/RawDataHero.jsx
@@ -7,11 +7,50 @@ const RawDataHero = () => {
 			<section className='data-hero-main'>
 				<header className='data-hero-header'>Raw Data</header>
 				<br />
-				<article className='data-hero-'>
-					<div className=''>
-						<i className='far fa-folder-open' />
-					</div>
-				</article>
+				<div className='data-hero-cards-wrapper'>
+					<span className='data-hero-card-spacer'>
+						<a
+							href='https://docs.google.com/spreadsheets/d/1vrtaarKEZpYJ3GE0BR3e92VapOtAVCEKV-FfT1heoho/edit#gid=379863631'
+							target='_blank'
+							rel='noopener noreferrer'
+						>
+							<article className='data-hero-card'>
+								<i className='far fa-folder-open' />
+								<p className='data-hero-card-title'>
+									Public Raw Data Sheet
+								</p>
+							</article>
+						</a>
+					</span>
+					<span className='data-hero-card-spacer'>
+						<a
+							href='https://docs.google.com/spreadsheets/d/1vrtaarKEZpYJ3GE0BR3e92VapOtAVCEKV-FfT1heoho/edit#gid=379863631'
+							target='_blank'
+							rel='noopener noreferrer'
+						>
+							<article className='data-hero-card'>
+								<i className='far fa-folder-open' />
+								<p className='data-hero-card-title'>
+									Public Raw Data Sheet
+								</p>
+							</article>
+						</a>
+					</span>
+					<span className='data-hero-card-spacer'>
+						<a
+							href='https://docs.google.com/spreadsheets/d/1vrtaarKEZpYJ3GE0BR3e92VapOtAVCEKV-FfT1heoho/edit#gid=379863631'
+							target='_blank'
+							rel='noopener noreferrer'
+						>
+							<article className='data-hero-card'>
+								<i className='far fa-folder-open' />
+								<p className='data-hero-card-title'>
+									Public Raw Data Sheet
+								</p>
+							</article>
+						</a>
+					</span>
+				</div>
 			</section>
 		</Fragment>
 	);
