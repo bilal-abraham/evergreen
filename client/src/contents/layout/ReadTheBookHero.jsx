3a5a936eebe3b1211effcf44eec8194677f841cc
bilal-abraham <bilal.abraham.717@gmail.com>
2021-12-23 17:46:55 -0800

    read the book page completed

--- client/src/contents/layout/ReadTheBookHero.jsx
+++ client/src/contents/layout/ReadTheBookHero.jsx
@@ -4,7 +4,14 @@ import '../css/ReadTheBookHero.css';
 const ReadTheBookHero = () => {
 	return (
 		<Fragment>
-			<h1>Read the Book</h1>
+			<section className='book-hero-main'>
+				<header className='book-hero-header'>
+					Book Writing in Progress...
+				</header>
+				<div className='book-hero-spin-wrapper'>
+					<i className='fas fa-cog fa-pulse' />
+				</div>
+			</section>
 		</Fragment>
 	);
 };
